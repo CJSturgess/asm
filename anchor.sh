f96d1677bd694ac5084bb782ef95c133b1af9d6b
1717119 MC <cj.sturgess@gmail.com>
2016-02-25 22:10:46 -0800

    things

--- anchor.sh
+++ anchor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if ["$HOSTNAME" == "vultr.guest"]
-    then
-        echo "Yeppers"
+if ["$HOSTNAME" = "vultr.guest"]; then
+    echo "Yes"
+else
+    echo "No"
 fi
 #cd /home/anchor/asm
+#rm anchor.sh
 #git pull
 #chmod +x anchor.sh
 #node anchor
